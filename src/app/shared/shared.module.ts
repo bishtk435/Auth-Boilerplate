062009ac95bcad1e43f462fee50dc079f5d11d7e
bishtk435 <bishtk435@gmail.com>
2020-09-29 12:55:53 +0530

    imported shared module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { InputFieldComponent } from './input-field/input-field.component';
 
 
@@ -7,10 +9,14 @@ import { InputFieldComponent } from './input-field/input-field.component';
 @NgModule({
   declarations: [InputFieldComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   exports: [
-    InputFieldComponent
+    InputFieldComponent,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class SharedModule { }
