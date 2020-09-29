a72163d8435c9ddb104d38059963a84cf8984735
bishtk435 <bishtk435@gmail.com>
2020-09-29 11:26:09 +0530

    initial commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { InputFieldComponent } from './input-field/input-field.component';
+
+
+
+@NgModule({
+  declarations: [InputFieldComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
