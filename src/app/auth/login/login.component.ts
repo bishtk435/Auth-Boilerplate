18c9ae1c5294c244d5e0c7dc846bc38bbd9813e1
bishtk435 <bishtk435@gmail.com>
2020-09-29 16:10:10 +0530

    input field component added

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm = this.fb.group({
+    email: '',
+    password: ''
+  });
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    console.log('this is onsubmit', this.loginForm);
+  }
+
+  onSubmit(): void{
+    console.log('this is onsubmit', this.loginForm);
   }
 
 }
--- src/app/shared/input-field/input-field.component.ts
+++ src/app/shared/input-field/input-field.component.ts
@@ -1,19 +1,46 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, forwardRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 @Component({
   selector: 'app-input-field',
   templateUrl: './input-field.component.html',
-  styleUrls: ['./input-field.component.css']
+  styleUrls: ['./input-field.component.css'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => InputFieldComponent),
+      multi: true
+    }
+  ]
 })
-export class InputFieldComponent implements OnInit {
+export class InputFieldComponent implements OnInit, ControlValueAccessor {
+  @Input() controlName: string;
+  @Input() labelName: string;
   @Input() type: string;
-  @Input() name: string;
-  @Input() placeholder: string;
-  @Input() lablename: string;
+
+  writeValue(value: any): void {
+    this.controlName = value;
+  }
+
+  propagateChange = (_: any) => {
+  }
+
+  registerOnChange(fn): void{
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(): void{
+  }
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  updateInputField(newValue): void{
+    console.log('this is update input field', newValue);
+    this.controlName = newValue;
+    this.propagateChange(this.controlName);
   }
 
 }
